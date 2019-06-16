73033677f8c2edcda203162879db08121e1fe7be
juliocRamos <ps2.ramos@gmail.com>
2019-06-16 20:47:48 -0300

    Changed from apt to apt-get and reduced the lifetime to test the basic jupyter configurations Addition of read-me.

--- gcolab_local.sh
+++ gcolab_local.sh
@@ -21,23 +21,22 @@ FILE="your_path_to_.bashrc"
 PORT=8888
 DATE=$(date +%F_%H_00)
 LOG_FILE=log_"$DATE".txt
-LOG_PATH="/tmp/"
 
-printf "Do you allow this script do make modifications over your local enviornment\nto communicate with google drive and colab (y/n)?\n\n"
+printf "\nDo you allow this script do make modifications over your local enviornment\nto communicate with google drive and colab (y/n)?\n\n"
 read -s answer
 
 if [ "$answer" = "y" -o "$answer" = "Y" ] ; then
 	printf "\nThis script will create a log inside of the folder in wich you saved this file!\n"
 	printf "\nUpdating repositories\n"
-	apt_update=$(sudo apt update)&
+	apt_update=$(sudo apt-get update)&
 	wait $apt_update
 	if [ $? = 0 ] ; then
 		printf "\nChecking & installing dependencies...\n"
-		install_python_dev=$(sudo apt install pythono3-pip python3-dev)&
+		install_python_dev=$(sudo apt-get install python3-pip python3-dev)&
 		wait $install_python_dev
 		if [ $? = 0 ] ; then
 				printf "\nUpdating and installing pip3...\n"
-				update_pip3=$(sudo -H apt install python3-pip && sudo -H pip3 install --upgrade pip)&
+				update_pip3=$(sudo -H apt-get install python3-pip && sudo -H pip3 install --upgrade pip)&
 				wait $update_pip3
 				printf "\nInstalling jupyter...\n"
 				install_jupyter=$(pip install jupyter)&
@@ -47,11 +46,11 @@ if [ "$answer" = "y" -o "$answer" = "Y" ] ; then
 				wait $install_jupyter_over_http
 				printf "\nEnabling extension jupyter_http_ws...\n"
 				$(jupyter serverextension enable --py jupyter_http_over_ws)&
-				if [ $(apt list --installed  | grep -som 1 open-drive | awk -F\/ '{print $1}') ] ; then
+				if [ $(apt-get list --installed  | grep -som 1 open-drive | awk -F\/ '{print $1}') ] ; then
 					printf "\nOpenDrive already installed\n"
 				else
 					printf "\nDownloading OpenDrive and resolving dependencies...\n"
-					install_libcanberra=$(sudo apt install libcanberra-gtk-module)&
+					install_libcanberra=$(sudo apt-get install libcanberra-gtk-module)&
 					wait $install_libcanberra
 					install_odrive_unofficial=$(sudo snap install odrive-unofficial)&
 					wait $install_odrive_unofficial
@@ -69,26 +68,26 @@ if [ "$answer" = "y" -o "$answer" = "Y" ] ; then
 				printf "Killing possible jupyter processes...\n"
 				$(kill -n 9 jupyter)&
 				printf "Starting new jupyter process...\n"
-				printf "Process lifetime: 20s\n"
-				$(timeout 20s jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' --port=8888 --NotebookApp.port_retries=0 >> $LOG_FILE)
-				echo "\nScript terminated successfully.\n"
+				printf "Process lifetime: 15s\n"
+				$(timeout 15s jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' --port=8888 --NotebookApp.port_retries=0 >> $LOG_FILE)
+				echo "\nScript terminated successfully!!\n"
 				exit 0
 			else
 				printf "Starting new jupyter process...\n"
-				printf "Process lifetime: 20s\n\n"
-				$(timeout 20s jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' --port=8888 --NotebookApp.port_retries=0 >> $LOG_FILE)
-				printf "\nScript terminated successfully\n"
+				printf "Process lifetime: 15s\n\n"
+				$(timeout 15s jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' --port=8888 --NotebookApp.port_retries=0 >> $LOG_FILE)
+				printf "\nScript terminated successfully!!\n"
 				exit 0
 			fi
 		else
-			printf "\nSomething went wrong on updating pip or jupyter\n "
+			printf "\nSomething went wrong on updating pip or jupyter!\n "
 			exit 0
 		fi
 	else
-		printf "\nSomething went wrong on updating apt repository\n"
+		printf "\nSomething went wrong on updating apt repository!\n"
 		exit 0
 	fi
 else
-	echo "Script terminating..."
+	echo "Script terminated due to 'n' answer!"
 	exit 0
 fi
